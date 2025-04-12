4b031bbb3334e4b359f2aa54117a36f0e14af9d9
jjeongsu <jeongsu.dev@gmail.com>
2025-04-12 22:28:43 +0900

    refactor : client-side에서 chatting list 를 패칭하도록 변경

--- src/app/(chat)/expert/chat/page.tsx
+++ src/app/(chat)/expert/chat/page.tsx
@@ -2,11 +2,9 @@ import getRooms, { GetRoomsResponse } from '@/apis/chat/getRooms';
 import ExpertChattingTemplate from '@/components/templates/chatTemplate/ExpertChattingTemplate';
 
 export default async function ExpertChatPage() {
-  const chatRoomList: GetRoomsResponse = await getRooms();
-
   return (
     <div className="w-full flex justify-center">
-      <ExpertChattingTemplate chatRoomList={chatRoomList} />
+      <ExpertChattingTemplate />
     </div>
   );
 }
