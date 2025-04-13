2792cc69158c1dbf96e02814cc77ed87e9c797e5
jjeongsu <jeongsu.dev@gmail.com>
2025-04-13 20:10:50 +0900

    feat : 채팅방 나가기 기능 추가

--- src/components/molecules/chat/ChattingButtonGroup.tsx
+++ src/components/molecules/chat/ChattingButtonGroup.tsx
@@ -5,17 +5,23 @@ import ModalContainer from '../modal/ModalContainer';
 import ContractModal, { FormValue } from '../modal/ContractModal';
 import { OfferResponseType } from '@/apis/offer/getOffer';
 import { ExpertType } from '@/apis/expert/getExpert';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { postContract, PostContractRequestType } from '@/apis/contract/postContract';
 import { useRouter } from 'next/navigation';
+import { useUserStore } from '@/store/userStore';
+import { APIBuilder } from '@/utils/APIBuilder';
 
 export default function ChattingButtonGroup({
   offerInfo,
   expertInfo,
+  roomId,
 }: {
   offerInfo: OfferResponseType;
   expertInfo: ExpertType;
+  roomId?: string | null;
 }) {
+  const member = useUserStore(state => state.member);
+  const currentUserEmail = member?.email || ''; // 현재 사용자의 이메일
   const [openContract, setOpenContract] = useState(false);
   const router = useRouter();
   const postContractMutation = useMutation({
@@ -39,6 +45,40 @@ export default function ChattingButtonGroup({
       endDate: formValue.serviceEndDate,
     });
   };
+  const queryClient = useQueryClient();
+  // 채팅방 나가기
+  const handleChatOut = async () => {
+    if (roomId) {
+      const response = confirm('채팅방을 나가시겠습니까?');
+      if (response) {
+        try {
+          const outResponse = await fetch(
+            `${process.env.NEXT_PUBLIC_LOCAL_SERVER_URL}/chatrooms/${roomId}/leave?username=${currentUserEmail}`,
+            {
+              method: 'POST',
+              headers: {
+                accept: 'application/json',
+              },
+              credentials: 'include',
+            },
+          );
+
+          if (!outResponse.ok) {
+            throw new Error(`Error: ${outResponse.status}`);
+          }
+
+          const result = await outResponse.text();
+          alert(result); // 서버에서 반환된 메시지 표시
+
+          // ['chatroomList'] 쿼리 무효화 및 새로고침
+          queryClient.invalidateQueries({ queryKey: ['chatRoomList'] });
+        } catch (error) {
+          console.error('채팅방 나가기 요청 실패:', error);
+          alert('채팅방 나가기에 실패했습니다. 다시 시도해주세요.');
+        }
+      }
+    }
+  };
 
   const contractInfo = {
     expertProfileImage: expertInfo.profileImage,
@@ -51,9 +91,7 @@ export default function ChattingButtonGroup({
     <div className="w-full flex gap-16">
       <StandardButton
         text="채팅방 나가기"
-        onClick={() => {
-          alert('아직  구현되지 않았습니다.');
-        }}
+        onClick={handleChatOut}
         disabled={false}
         size="full"
         state="default"
